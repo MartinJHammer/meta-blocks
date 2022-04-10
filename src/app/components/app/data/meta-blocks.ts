6853439a8d94fadd0ff83cdad9a424c75c80ef9e
Martin Jul Hammer <martin_hammer@live.dk>
2022-04-10 12:36:47 +0200

    feat: add dummy data

---
+++ src/app/components/app/data/meta-blocks.ts
@@ -0,0 +1,22 @@
+import { Metablock } from '../models/meta-block';
+
+export const META_BLOCKS: Metablock[] = [
+    {
+        id: '1',
+        name: 'My content',
+        content: 'This is some default content',
+        meta: {}
+    },
+    {
+        id: '2',
+        name: 'My content 2',
+        content: 'This is some default content 2',
+        meta: {}
+    },
+    {
+        id: '3',
+        name: 'My content 3',
+        content: 'This is some default content 3',
+        meta: {}
+    },
+];
