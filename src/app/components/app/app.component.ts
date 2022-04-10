c5717d6671b56f86f1cccf2346a15e15f93790a4
Martin Jul Hammer <martin_hammer@live.dk>
2022-04-10 12:37:31 +0200

    feat: use new utils

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { shuffleNumbers, withLog } from './utils/logic';
+import { META_BLOCKS } from './data/meta-blocks';
+import { log, pipe, withLog } from './utils/base';
+import { noNillElements } from './utils/misc';
+import { shuffleNumbers } from './utils/randomizers';
+import { upperCaseArray } from './utils/strings';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +12,19 @@ import { shuffleNumbers, withLog } from './utils/logic';
 })
 export class AppComponent {
   title = 'MetaBlocks';
+  metBlocks = META_BLOCKS;
 
   ngOnInit(): void {
     const shuffleWithLog = withLog(shuffleNumbers);
 
     shuffleWithLog([1,2,3,4,5,6,7,8,9,10]);
+    
+    const processStrings = pipe<string[]>(
+      noNillElements<string[]>('[processStrings]: val was nill'),
+      upperCaseArray,
+      log('Results:')
+    );
+
+    processStrings(['test', 'test', 'test']);
   }
 }
