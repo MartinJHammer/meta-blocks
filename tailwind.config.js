0adfed66abf9ba3cdc837ba97bb4d570f69c1124
Martin Jul Hammer <martin_hammer@live.dk>
2022-04-11 12:23:55 +0200

    feat: tailwind class - no scrollbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,39 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
-  content: ["./src/**/*.{html,js,ts}"],
+  content: ['./src/**/*.{html,js,ts}'],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        '.no-scrollbar': {
+          /* IE and Edge */
+          '-ms-overflow-style': 'none',
+
+          /* Firefox */
+          'scrollbar-width': 'none',
+
+          /* Safari and Chrome */
+          '&::-webkit-scrollbar': {
+            display: 'none',
+          },
+        },
+
+        '.scrollbar-default': {
+          /* IE and Edge */
+          '-ms-overflow-style': 'auto',
+
+          /* Firefox */
+          'scrollbar-width': 'auto',
+
+          /* Safari and Chrome */
+          '&::-webkit-scrollbar': {
+            display: 'block',
+          },
+        },
+      });
+    }),
+  ],
+};
