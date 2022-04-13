881385ad82af6f3d762e7cf7eae146586343e00b
Martin Jul Hammer <martin_hammer@live.dk>
2022-04-13 12:49:07 +0200

    feat: add math utils

---
+++ src/app/components/app/utils/math.ts
@@ -0,0 +1,4 @@
+export const multiply = (...numbers: number[]): number => numbers.reduce((a, b) => a * b);
+export const add = (...numbers: number[]): number => numbers.reduce((a, b) => a + b);
+export const subtract = (...numbers: number[]): number => numbers.reduce((a, b) => a - b);
+export const divide = (...numbers: number[]): number => numbers.reduce((a, b) => a / b);
